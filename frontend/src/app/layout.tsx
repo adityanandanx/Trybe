18e7127bb9f0f35d5c612c9336d9ed938a6cbfd0
adityanandanx <nandanaditya985@gmail.com>
2024-08-11 23:03:23 +0530

    feat: add basic components

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Trybe - Extensive Reach and Seamless Coordination for your club",
@@ -23,6 +24,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
